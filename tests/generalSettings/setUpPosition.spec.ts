af06f4bd36bd00dfdbe31dd3a8cbc06b6fd4c585
Pongsakorn <121147244+ohmicon@users.noreply.github.com>
2024-06-18 18:11:03 +0700

    Add test case SetUpPosition and SetUpPermissionProject

---
+++ tests/generalSettings/setUpPosition.spec.ts
@@ -0,0 +1,34 @@
+import { test } from "@playwright/test";
+import dataSetting from "../../data/common/TestSetting.json"
+import { LoginPage } from "../../src/pages/common/login-page"
+import { setUpPositionData } from "../../data/generalSettings/setUpPosition.js"
+
+const dataLogin = dataSetting.Login;
+const data = setUpPositionData;
+
+test.describe("Setup Position", async () => {
+    test("Setup Position", async ({ page }) => {
+        await page.goto(dataLogin.site);
+        const loginPage = new LoginPage(page);
+        await loginPage.goto(dataLogin.site);
+        await loginPage.login(dataLogin.username, dataLogin.password);
+        await page.waitForLoadState("load");
+        await page
+          .getByRole("link", { name: "ระบบจัดการข้อมูลกลาง" })
+          .click({ force: true });
+        await page.locator('a').filter({ hasText: 'Genaral Settings' }).click();
+        await page.getByRole('link', { name: ' Setup Model Type' }).click();
+        await page.getByRole('button', { name: ' New' }).click();
+        await page.locator('#model_type_code').click();
+        await page.locator('#model_type_code').fill(data.modelTypeCode);
+        await page.locator('#model_type_name').click();
+        await page.locator('#model_type_name').fill(data.modelTypeName);
+        await page.locator('#model_type_name_eng').click();
+        await page.locator('#model_type_name_eng').fill(data.modelTypeNameEN);
+        await page.locator('#short_name').click();  
+        await page.locator('#short_name').fill(data.modelTypeShortName);  
+        await page.locator('#project_type').selectOption(data.projectType);
+        await page.locator('#save_t').click();
+        await page.getByRole('button', { name: 'OK' }).click();
+    })
+})
