d20dfe8e1e0e0f3ac64872080f6d96494330945c
Tannaporn1811 <100668918+Tannaporn1811@users.noreply.github.com>
2024-06-25 19:26:41 +0700

    Add page\setapprove-paget.ts

--- src/pages/setapprove-page.ts
+++ src/pages/setapprove-page.ts
@@ -1,13 +1,40 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 import SetupApprove from "../../data/generalSettings/setupApprove.js";
+import dataSetting from "../../data/common/TestSetting.json";
 
+const data = dataSetting.Login;
 const setGroupData = SetupApprove[0]
+
+export const chooseMenu=async(page:Page)=>{
+   
+    await page.locator("#ascrail2000 div").click({ force: true });
+    await page.mouse.wheel(500, 0);
+
+    await page
+      .getByRole("link", { name: "Setup Project & Department" })
+      .click({ force: true });
+    await page
+      .getByRole("link", { name: "Setup Approve" })
+      .click({ force: true });
+}
+export const setupUserApproveGroup = async (page: Page) => {
+   //Save Group
+   await page.getByText("Add Group").click({ force: true });
+   await page.locator("#groupname").click({ force: true });
+   await page.locator("#groupname").fill(setGroupData.groupName);
+   await page.locator("#remark").click({ force: true });
+   await page.locator("#remark").fill("for test iot", { force: true });
+   await page.locator("#department").selectOption(setGroupData.department);
+   await page.getByText("Save Group").click({ force: true });
+   //Save Group
+  };
+
 
 export const setupUserApprove = async (page: Page) => {
     await page.locator('input[type="search"]').click({ force: true });
   
-    await page.getByLabel('Filter:').fill(setGroupData.groupName);
-    await page.getByLabel('Filter:').press('Enter');
+    await page.locator('input[type="search"]').fill(setGroupData.groupName);
+    await page.locator('input[type="search"]').press('Enter');
     await page.getByRole('gridcell', { name: '      ' }).locator('a').first().click({ force: true });
 
     //Add row
@@ -16,13 +43,7 @@ export const setupUserApprove = async (page: Page) => {
     await page.waitForLoadState();      
     await page.getByLabel('Search:').click({ force: true });
     await page.getByLabel('Search:').fill(setGroupData.user);
-    await page.getByLabel('Search:').press('Tab');
-    await page.getByLabel('No.: activate to sort column').press('Tab');
-    await page.getByLabel('Project/Department: activate').press('Tab');
-    await page.getByRole('row', { name: 'No.: activate to sort column' }).getByLabel('Active: activate to sort').press('Tab');
-    await page.getByRole('button', { name: 'SELECT' }).click({ force: true });
-    
+    await page.locator('button').filter({hasText:'SELECT'}).click({ force: true });
     await page.getByText('Save', { exact: true }).click();
-   await page.getByRole('heading', { name: 'Save Completed !' }).click({ force: true });
-   await page.getByRole('button', { name: 'OK' }).click({ force: true });
+   await page.locator('button').filter({hasText:'OK'}).click({ force: true });
   };
